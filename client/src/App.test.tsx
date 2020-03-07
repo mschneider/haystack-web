381788c1cee7e62774c68865a672dfb229f3ea24
Maximilian Schneider <mail@maximilianschneider.net>
2020-03-07 17:56:04 +0100

    move websearch client into its own repo and host build output instead

--- client/src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
-
-test('renders Haystack title', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/Haystack/i);
-  expect(linkElement).toBeInTheDocument();
-});
---
+++ search/precache-manifest.a350b17453a749fbc537881f64374627.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81da4e90ed373d0d29a051337eec7eb6",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e8c4c1be45f1b2ff64c1",
+    "url": "./static/css/main.8fdd633e.chunk.css"
+  },
+  {
+    "revision": "7bc368cffd56b2a07154",
+    "url": "./static/js/2.ccbd49ce.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "./static/js/2.ccbd49ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8c4c1be45f1b2ff64c1",
+    "url": "./static/js/main.ee23aa5a.chunk.js"
+  },
+  {
+    "revision": "70f774eb50715ed63f0a",
+    "url": "./static/js/runtime-main.e8cdf570.js"
+  }
+]);
